bca54475a2466ff80962502332c8b3e2c53cc723
EpperlaKarthik <52861755+EpperlaKarthik@users.noreply.github.com>
2020-05-03 16:25:11 +0530

    Add files via upload

---
+++ Scripts/README.md
@@ -0,0 +1,9 @@
+# DesertShooter
+A Virtual Reality Prototype of Whack A Mole game, build using Unity3D, Photon PUN, Google VR SDK and Google Firebase.
+
+
+# To Make Changes
+Clone the repo and open with unity version 2018.3.5f1. Make sure you use the same version of Google VR SDK as well, change the build platfrom (Android/IOS) to your convinience.
+
+
+
---
+++ Scripts/SCENECHANGE.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SCENECHANGE : MonoBehaviour
+{
+   public void oneto2()
+    {
+        SceneManager.LoadScene("2");
+    }
+    public void twotohome3()
+    {
+        SceneManager.LoadScene("3");
+    }
+    public void threeto4()
+    {
+        SceneManager.LoadScene("4");
+    }
+    public void fourto5()
+    {
+        SceneManager.LoadScene("5");
+    }
+    public void fourto6()
+    {
+        SceneManager.LoadScene("6");
+    }
+    public void sixto4()
+    {
+        SceneManager.LoadScene("4");
+    }
+    public void fiveto4()
+    {
+        SceneManager.LoadScene("4");
+    }
+   
+    public void fiveto7()
+    {
+        SceneManager.LoadScene("7");
+    }
+    public void sixto8()
+    {
+        SceneManager.LoadScene("8");
+    }
+    public void seventogame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void sevento5()
+    {
+        SceneManager.LoadScene("5");
+    }
+    public void eightto6()
+    {
+        SceneManager.LoadScene("8");
+    }
+   
+    
+}
+
+
